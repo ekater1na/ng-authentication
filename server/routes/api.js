453016e4b9cd823561d4c4bdc052b909881879da
ekater1na <ekaterina_l@tut.by>
2020-01-30 16:42:18 +0300

    feat: add API registration

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
+consr User = require('../models/user')
 const mongoose = require('mongoose');
-
 const db = 'mongodb://uservishwas:uservishwas@cluster0-shard-00-00-lfxp5.mongodb.net:27017,cluster0-shard-00-01-lfxp5.mongodb.net:27017,cluster0-shard-00-02-lfxp5.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true}, err => {
@@ -11,6 +11,18 @@ mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true}, err => 
         console.log('Connected to mongodb')
     }
 });
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let User = new User(userData)
+    userData.save((error, registerUser) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.status(200).send(registerUser)
+        }
+    })
+})
 
 router.get('/', (req, res) => {
     res.send('From API route')
