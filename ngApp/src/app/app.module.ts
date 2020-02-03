b3b0e78cfc6cbc9077e283b315246302c61d8cf6
ekater1na <ekaterina_l@tut.by>
2020-02-03 16:40:42 +0300

    build: add auth service

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,7 +23,8 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
