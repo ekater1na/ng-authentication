bf7428e7fb60d4841a4dfc0210eac67017477b56
ekater1na <ekaterina_l@tut.by>
2020-02-04 18:43:54 +0300

    feat: add Login UI

--- ngApp/src/app/login/login.component.ts
+++ ngApp/src/app/login/login.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginUserData: any = {}
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginUser() {
+    console.log(this.loginUserData);
+    
   }
 
 }
